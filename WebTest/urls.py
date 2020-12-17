1c91ff244fa54998b9f8375a24c92745e01eb9c7
amos@rpi <805780535@qq.com>
2020-12-17 23:58:58 +0800

    rm wrong app

--- WebTest/urls.py
+++ WebTest/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.mainpage),
+    url('login/', include('login.urls'))
 ]
